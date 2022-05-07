43598d64c54170cf49b821ff9197cf014756f1c4
Nati Kamusher <natan.kamusher@gmail.com>
2022-05-07 18:04:24 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/Hi There!!/i);
+	expect(linkElement).toBeInTheDocument();
 });
